367249653228b942bd2d756ae119170dd2c1aa53
Amalcxc <amalcxirsi@gmail.com>
2021-04-05 21:21:23 +0300

    first TUTORIAL answers

---
+++ answers.sql
@@ -0,0 +1,17 @@
+
+/*
+*****TUTORIAL 1*****
+*/
+
+SELECT population FROM world
+  WHERE name = 'Germany'
+
+SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway', 'Denmark');
+
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000  AND 250000
+
+  /*
+*****TUTORIAL 2*****
+*/
