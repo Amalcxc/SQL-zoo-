6c4a2893a0de5c6ed6f6b4d690d04c261f91ebd5
Amalcxc <amalcxirsi@gmail.com>
2021-04-05 23:53:37 +0300

    second

--- answers.sql
+++ answers.sql
@@ -15,3 +15,60 @@ SELECT name, area FROM world
   /*
 *****TUTORIAL 2*****
 */
+
+SELECT name, continent, population FROM world
+
+SELECT name
+  FROM world
+ WHERE population > 200000000
+
+ SELECT name, gdp/population
+  FROM world
+ WHERE population > 200000000
+
+ SELECT name, population/1000000
+  FROM world
+ WHERE continent = 'South America'
+
+ SELECT name, population
+  FROM world
+ WHERE name IN ('France','Germany','Italy');
+
+ SELECT name
+  FROM world
+ WHERE name LIKE '%United%'
+
+SELECT name, population, area
+  FROM world
+ WHERE area > 3000000 OR population >= 250000000
+ 
+SELECT name, population, area
+  FROM world
+ WHERE (area > 3000000 AND population < 250000000)
+    OR (area < 3000000 AND population > 250000000)
+
+
+SELECT name, ROUND(population/1000000,2), ROUND(gdp/1000000000,2)
+  FROM world
+ WHERE continent = 'South America'
+
+ SELECT name, ROUND(gdp/population,-3)
+  FROM world
+ WHERE gdp > 1000000000000
+
+ SELECT name,
+       capital
+  FROM world
+ WHERE LEN(name) LIKE LEN(capital)
+
+ SELECT name, capital 
+FROM world
+WHERE LEFT(name,1) = LEFT(capital,1) AND name !=capital
+
+SELECT name
+   FROM world
+WHERE name LIKE '%e%' and 
+      name LIKE '%i%' and
+      name LIKE '%o%' and
+      name LIKE '%u%'    
+  AND name NOT LIKE '% %'
